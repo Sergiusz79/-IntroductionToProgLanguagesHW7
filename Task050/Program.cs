3abc2112fc2852dde2cef5868c16b8ee7b21d9c6
Siarhei Davidzenka <mc.sergey30@gmail.com>
2023-05-21 21:56:04 +0200

    Task0052

--- Task050/Program.cs
+++ Task050/Program.cs
@@ -5,3 +5,57 @@
                         //8 4 2 4
 
 
+int Enter(string message)
+{
+    int n;
+        Console.WriteLine($"Please enter the number of the {message}: ");
+        while (!int.TryParse(Console.ReadLine(), out n))
+        {Console.WriteLine("It's not number! Please enter number!");}
+    return n;
+} 
+
+void FillArray(int[,] numbers)
+{
+Random rnd = new Random();
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+        numbers[i, j] = rnd.Next(-9, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] numbers)
+{
+   for (int i = 0; i < numbers.GetLength(0); i++) 
+   { 
+        for (int j = 0; j < numbers.GetLength(1); j++) 
+        { 
+            Console.Write($"{numbers[i, j]} "); 
+        } 
+        Console.WriteLine(); 
+    } 
+}
+
+void Checking(int[,] numbers)
+{
+ int str = Enter("strings");
+ int tab = Enter("tables");
+    if (str <= numbers.GetLength(0) && tab <= numbers.GetLength(1))
+        {Console.WriteLine($"The value of the given array element {numbers[str, tab]} ");}
+    else 
+        {Console.WriteLine($"There is no such cell in the array!");}
+}
+
+void Task050()
+{
+ int[,] numbers = new int[3, 4];
+ FillArray(numbers);
+ Checking(numbers);
+ Console.WriteLine();
+ PrintArray(numbers);
+}
+
+Task050();
--- Task052/Program.cs
+++ Task052/Program.cs
@@ -5,5 +5,59 @@
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
+void FillArray(double[,] numbers)
+{
+Random rnd = new Random();
+
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        for (int j = 0; j < numbers.GetLength(1); j++)
+        {
+        numbers[i, j] = rnd.Next(1, 11);
+        }
+    }
+}
+
+void PrintArray(double[,] numbers)
+{
+   for (int i = 0; i < numbers.GetLength(0); i++) 
+   { 
+        for (int j = 0; j < numbers.GetLength(1); j++) 
+        { 
+            Console.Write($"{numbers[i, j]}  "); 
+        } 
+        Console.WriteLine(); 
+    } 
+}
+
+void Average (double[,] array)
+{
+ double[] av = new double[array.GetLength(1)];
+
+   for (int j = 0; j < array.GetLength(1); j++) 
+   { 
+        double sum = 0;
+        for (int i = 0; i < array.GetLength(0); i++) 
+        { 
+            sum += array[i, j];
+        }
+        av[j] = sum/array.GetLength(0); 
+        Console.Write($"{av[j]}  "); 
+    } 
+ Console.WriteLine(); 
+}
+
+void Task052()
+{
+ double[,] numbers = new double[3, 4];
+ double[] av = new double[numbers.GetLength(0)];
+ FillArray(numbers);
+ Average(numbers);
+ Console.WriteLine();
+ PrintArray(numbers);
+}
+
+Task052();
+
 
 
